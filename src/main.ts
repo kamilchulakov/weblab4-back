59b45bace29302c666d18815c89899fef3acbd13
ulyanovskk <mr1english4@gmail.com>
2021-12-19 16:27:59 +0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('WebLab 4')
+    .setDescription('WebLab 4 API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
   console.log('Running on port: 3000');
 }
