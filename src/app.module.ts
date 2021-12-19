a4a860953631c4d6fe81ce141307fb59061778f3
ulyanovskk <mr1english4@gmail.com>
2021-12-19 15:16:31 +0300

    Update db connection with .env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: process.env.USER,
+      username: process.env.DB_USER,
       password: process.env.PASSWORD,
       database: process.env.DB,
       models: [],
