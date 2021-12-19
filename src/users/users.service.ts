4c0bab29d6e72f2c74b51a50dcf8221855c4928a
ulyanovskk <mr1english4@gmail.com>
2021-12-19 21:36:43 +0300

    Fix bad login

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { UserCreateDto } from './dto/user.create.dto';
@@ -13,11 +13,15 @@ export class UsersService {
   ) {}
 
   async createUser(dto: UserCreateDto) {
-    const user = await this.userRepository.create(dto);
-    const role = await this.rolesService.getRoleByName('USER');
-    await user.$set('roles', [role.id]);
-    user.roles = [role];
-    return user;
+    try {
+      const user = await this.userRepository.create(dto);
+      const role = await this.rolesService.getRoleByName('USER');
+      await user.$set('roles', [role.id]);
+      user.roles = [role];
+      return user;
+    } catch (e) {
+      throw new HttpException('Login already in use!', HttpStatus.BAD_REQUEST);
+    }
   }
   async getAllUsers() {
     const users = await this.userRepository.findAll({ include: Role });
