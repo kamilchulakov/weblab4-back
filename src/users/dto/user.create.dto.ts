dfee2a23cf295e7e649bb124a989e00f12b50564
ulyanovskk <mr1english4@gmail.com>
2021-12-21 14:53:32 +0300

    Add validations to UserCreateDto

--- src/users/dto/user.create.dto.ts
+++ src/users/dto/user.create.dto.ts
@@ -1,8 +1,14 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length } from 'class-validator';
 
 export class UserCreateDto {
   @ApiProperty({ example: 'user', description: 'Unique user login' })
+  @IsString({ message: 'Must be a string' })
+  @Length(4, 16, {
+    message: 'Length of login must be more than 4 and less than 16',
+  })
   readonly login: string;
+  @Length(4, 30, { message: 'Length of login must be more than 4 and less than 30' })
   @ApiProperty({ example: 'changeme', description: 'User password' })
   readonly password: string;
 }
